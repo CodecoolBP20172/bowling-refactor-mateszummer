361544b01515175e05197efe34409d0978de8052
mateszummer <szummerm@gmail.com>
2017-08-04 14:40:24 +0200

    dead code removed

--- bowling.py
+++ bowling.py
@@ -7,25 +7,20 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
+            result += get_value(game[i+1])
+            if game[i].upper() == 'X':
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+            frame += 1
+        if game[i].upper() == 'X':
             in_first_half = True
             frame += 1
     return result
@@ -35,7 +30,7 @@ def get_value(char):
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
         return int(char)
-    elif char == 'X' or char == 'x':
+    elif char.upper() == 'X':
         return 10
     elif char == '/':
         return 10
