1f8a3c660439074acd0d84a56949624734ace6be
mateszummer <szummerm@gmail.com>
2017-08-05 11:29:14 +0200

    few more deadcodes removed / faulty fixed

--- bowling.py
+++ bowling.py
@@ -4,7 +4,7 @@ def score(game):
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            result += 10 - get_value(game[i-1])
         else:
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
@@ -14,21 +14,20 @@ def score(game):
                     result += 10 - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
-        last = get_value(game[i])
         if in_first_half:
             in_first_half = False
+            if game[i].upper() == 'X':
+                in_first_half = True
+                frame += 1
         else:
-            in_first_half = True
-            frame += 1
-        if game[i].upper() == 'X':
             in_first_half = True
             frame += 1
     return result
 
 
 def get_value(char):
-    chars = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
-             "7": 7, "8": 8, "9": 9, "X": 10, "/": 10, "-": 0}
+    chars = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, \
+            "7": 7, "8": 8, "9": 9, "X": 10, "/": 10, "-": 0}
     if char.upper() in chars:
         return chars[char.upper()]
     else:
